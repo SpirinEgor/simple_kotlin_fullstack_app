df37726c501a0dd6c32bdbf8c54ecfab6e5b5bce
Egor Spirin <spirin.egor@mail.ru>
2021-12-07 01:51:54 +0300

    Setup version for ktor

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,7 +1,7 @@
 group = rootProject.group
 version = rootProject.version
 
-val ktorVersion = "1.6.6"
+val ktorVersion = "1.6.4"
 
 plugins {
     kotlin("jvm") version "1.6.0"
@@ -22,12 +22,6 @@ dependencies {
 }
 
 tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
     test {
         useJUnitPlatform()
         testLogging {
